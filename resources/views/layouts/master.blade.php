6b3f1796030a5281f7c853dd95e367465a503ee7
bellweerin <weerin.n@ku.th>
2020-03-07 10:49:07 +0700

    add before pull

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -17,7 +17,7 @@
     @include('layouts.menu')
 
 
-    <div class="">
+    <div class="container">
         @yield('content')
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master')
+@section('style')
+
+@endsection
+
+@section('content')
+
+
+@endsection
+
+@section('script')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+@endsection
