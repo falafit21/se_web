00e5f5c1f67a60167a32675dbc3211e054b52e89
6110406147 <phitchayatida.c@ku.th>
2020-03-08 20:47:19 +0700

    update

--- resources/views/pets/create.blade.php
+++ resources/views/pets/create.blade.php
@@ -2,28 +2,37 @@
 
 @section('content')
     <div style="margin-right: 200px; margin-left: 200px; margin-top: 50px">
-        <h4>Create Pet</h4>
+        <h4 style="text-align:center;">Create Pet</h4>
         <form>
-            <div class="form-group">
-                <label for="genre">Genre</label>
-                <select id="choosePet" class="form-control">
-                    <option value="0">dog</option>
-                    <option value="1">cat</option>
-                    <option value="2" selected="selected">giraffe</option>
-                </select>
+            <div class="form-group row">
+                <label for="genre"  class="col-2 col-form-label">Genre</label>
+                <div class="col-10">
+                     <select id="choosePet" class="form-control">
+                         <option value="0">dog</option>
+                         <option value="1">cat</option>
+                         <option value="2" selected="selected">giraffe</option>
+                     </select>
+                </div>
+
             </div>
-            <div class="form-group">
-                  <label for="size">Size</label>
-                   <select id="choosePet" class="form-control">
-                      <option value="0">Small</option>
-                      <option value="1">Medium</option>
-                      <option value="2">Large</option>
-                      <option value="3" selected="selected">Giants</option>
-                  </select>
+            <div class="form-group row">
+                  <label for="size" class="col-2 col-form-label">Size</label>
+                  <div class="col-10">
+                      <select id="choosePet" class="form-control">
+                           <option value="0">Small</option>
+                           <option value="1">Medium</option>
+                           <option value="2">Large</option>
+                           <option value="3" selected="selected">Giants</option>
+                      </select>
+                   </div>
+
              </div>
-            <div class="form-group">
-                <label for="name">Name</label>
-                <input type="name" class="form-control" id="name">
+
+            <div class="form-group row">
+                <label for="name" class="col-2 col-form-label">Name</label>
+                 <div class="col-10">
+                    <input type="name" class="form-control" id="name">
+                </div>
             </div>
 
             <div class="form-group row">
@@ -33,15 +42,13 @@
                 <small id="fileHelp" class="form-text text-muted"></small>
               </div>
             </div>
+
             <div class="form-group row">
                 <label for="age"class="col-2 col-form-label" >Age</label>
                 <div class="col-10">
                     <input type="text" class="form-control" id="age">
                     <small id="fileHelp" class="form-text text-muted">Please answer in week(s)</small>
-
                 </div>
-
-
              </div>
 
 
@@ -56,15 +63,88 @@
 
             </div>
 
-            <div class="form-group">
-                <label for="exampleInputFile">File input</label>
+            <div class="form-group row">
+                <label for="exampleInputFile" class="col-2 col-form-label">Pet Image</label>
+                <div class="col-10">
                 <input type="file" class="form-control-file" id="exampleInputFile" aria-describedby="fileHelp">
               </div>
-              <div class="form-group row">
-                    <div class="offset-sm-11">
-                      <button type="submit" class="btn btn-primary">Create</button>
-                    </div>
-                  </div>
+
+            <div class="form-group row">
+                                <div class="offset-sm-11">
+                                  <button type="submit" class="btn btn-primary">Create</button>
+                                </div>
+                              </div>
+
         </form>
     </div>
+//    <div style="margin-right: 200px; margin-left: 200px; margin-top: 50px">
+//                   <h4>Create Pet</h4>
+//                   <form>
+//                       <div class="form-group">
+//                           <label for="genre">Genre</label>
+//                           <select id="choosePet" class="form-control">
+//                               <option value="0">dog</option>
+//                               <option value="1">cat</option>
+//                               <option value="2" selected="selected">giraffe</option>
+//                           </select>
+//                       </div>
+//                       <div class="form-group">
+//                             <label for="size">Size</label>
+//                              <select id="choosePet" class="form-control">
+//                                 <option value="0">Small</option>
+//                                 <option value="1">Medium</option>
+//                                 <option value="2">Large</option>
+//                                 <option value="3" selected="selected">Giants</option>
+//                             </select>
+//                        </div>
+//                       <div class="form-group">
+//                           <label for="name">Name</label>
+//                           <input type="name" class="form-control" id="name">
+//                       </div>
+//
+//                       <div class="form-group row">
+//                         <label for="example-date-input" class="col-2 col-form-label">BirthDate</label>
+//                         <div class="col-10">
+//                           <input class="form-control" type="date" value="2011-08-19" id="example-date-input">
+//                           <small id="fileHelp" class="form-text text-muted"></small>
+//                         </div>
+//                       </div>
+//                       <div class="form-group row">
+//                           <label for="age"class="col-2 col-form-label" >Age</label>
+//                           <div class="col-10">
+//                               <input type="text" class="form-control" id="age">
+//                               <small id="fileHelp" class="form-text text-muted">Please answer in week(s)</small>
+//
+//                           </div>
+//
+//
+//                        </div>
+//
+//
+//                       <div class="form-group row">
+//                            <label for="weight" class="col-2 col-form-label">Weight</label>
+//                            <div class="col-10">
+//                                <input type="text" class="form-control" id="weight">
+//                                <small id="fileHelp" class="form-text text-muted"> Please answer in Kilograms Unit</small>
+//
+//                            </div>
+//
+//
+//                       </div>
+//
+//                       <div class="form-group">
+//                           <label for="exampleInputFile">File input</label>
+//                           <input type="file" class="form-control-file" id="exampleInputFile" aria-describedby="fileHelp">
+//                         </div>
+//                         <div class="form-group row">
+//                               <div class="offset-sm-11">
+//                                 <button type="submit" class="btn btn-primary">Create</button>
+//                               </div>
+//                             </div>
+//                   </form>
+//               </div>
 @endsection
+
+
+
+
