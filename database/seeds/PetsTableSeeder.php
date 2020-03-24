75a6e1d143098427a972f9ed3a5a0bda0ba5f721
6110406147 <phitchayatida.c@ku.th>
2020-03-24 10:38:19 +0700

    update

--- database/seeds/PetsTableSeeder.php
+++ database/seeds/PetsTableSeeder.php
@@ -11,13 +11,6 @@ class PetsTableSeeder extends Seeder
      */
     public function run()
     {
-        $pet = new \App\Pet();
-        $pet->name = "Bobo";
-        $pet->user_id = 1;
-        $pet->pet_type_id = 1;
-        $pet->pet_gene_id = 1;
-        $pet->weight = 25.2;
-        $pet->birth_date = "1997-10-10";
-        $pet->save();
+
     }
 }
